c83ea6779a88efccfe8a4553f5b297cd7c8f10c4
2byte <oopsfix21@gmail.com>
2025-01-24 06:53:40 +0300

    up

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,6 +4,7 @@ import { useForm } from "@inertiajs/vue3";
 import { ref, reactive, watch, watchEffect } from "vue";
 import FormPresenter from "@/Components/FormPresenter/FormPresenter.vue";
 import { FormPresenter as FormPresenterBuilder } from "@/Components/FormPresenter/FormPresenter";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 
 const poviderCreateForm = useForm({
     name: null,
@@ -27,6 +28,8 @@ const form = FormPresenterBuilder.init()
     });
 
 const presenter = form.make();
+
+const isShowCreateProviderForm = ref(false);
 </script>
 
 <template>
@@ -39,14 +42,18 @@ const presenter = form.make();
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow sm:rounded-lg">
+                <div class="bg-white overflow-hidden shadow sm:rounded-lg mb-2">
                     <div class="p-12 text-xl">
                         Welcome to test frutokassa.ru for payment provider
                     </div>
                 </div>
 
-                <div class="p-12">
-                    <FormPresenter :presenter />
+                <div>
+                    <PrimaryButton v-if="!isShowCreateProviderForm" @click="isShowCreateProviderForm = !isShowCreateProviderForm">Создать провайдера</PrimaryButton> 
+                </div>
+
+                <div class="p-12 md:w-1/4" v-if="isShowCreateProviderForm">
+                    <FormPresenter :presenter/>
                 </div>
             </div>
         </div>
